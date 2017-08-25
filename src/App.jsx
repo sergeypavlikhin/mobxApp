5c5f8deacfd421f2a296c55c8dadd827ac8b02e8
s.pavlikhin <sergey.pavlikhin@inbox.ru>
2017-08-25 18:48:24 +0300

    Continue learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import mobx from 'mobx'
+import mobx_react from 'mobx-react'
 
+@observer
 class App extends React.Component {
+
+    state = {
+      names: []
+    };
+
     constructor(props){
         super(props);
+        this.joinNames  = this.joinNames.bind(this);
+        this.addName    = this.addName.bind(this);
+
     }
+
+    addName(){
+        let names = this.state.names;
+        names.push("newName");
+        this.setState({names});
+    }
+
     render(){
         return (
             <div>
                 <h1>
-                    Hello me
+                    Hello me {this.joinNames()}
                 </h1>
+                <button onClick={this.addName}>
+                    NewName
+                </button>
             </div>
         );
+    }
+    joinNames(){
+        return this.state.names.join(" | ");
     }
 }
 
