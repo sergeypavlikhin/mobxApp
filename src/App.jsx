d391a6f19706cfc22cd79f4aa3b76fbcd63308bd
s.pavlikhin <sergey.pavlikhin@inbox.ru>
2017-08-28 16:00:41 +0300

    Continue learn mobx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,71 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import mobx from 'mobx'
-import mobx_react from 'mobx-react'
 
-@observer
-class App extends React.Component {
+import {autorun, observable, computed, action} from "mobx"
+import { observer } from 'mobx-react';
 
-    state = {
-      names: []
-    };
+class Todo{
 
-    constructor(props){
-        super(props);
-        this.joinNames  = this.joinNames.bind(this);
-        this.addName    = this.addName.bind(this);
+    @observable name = "";
+    @observable duration = 0;
 
+    constructor(name, duration){
+        this.name = name;
+        this.duration = duration;
     }
 
-    addName(){
-        let names = this.state.names;
-        names.push("newName");
-        this.setState({names});
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>
-                    Hello me {this.joinNames()}
-                </h1>
-                <button onClick={this.addName}>
-                    NewName
-                </button>
-            </div>
-        );
-    }
-    joinNames(){
-        return this.state.names.join(" | ");
+    toString(){
+        return `Todo:[name: ${this.name}, duration: ${this.duration} ]`;
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+class TodoList {
+
+    @observable store = [];
+
+    get report(){
+        return this.store.reduce((init, current) => init + "\n" + current.toString());
+    }
+
+    @action
+    add(todo) {
+        this.store.push(todo);
+    }
+
+}
+
+
+let list = new TodoList;
+list.add(new Todo("work", "8h"));
+
+@observer
+class App extends React.Component {
+    render(){
+        const store = this.props.store;
+        return (
+            <div>
+                <div>
+                    { store.store.map((item) => <TodoView todo={ item } key={item.name}/>)}
+                </div>
+                <button onClick={ this.onNewTodo }>CLIKME</button>
+            </div>
+
+        );
+    }
+    onNewTodo = () => {
+        this.props.store.add(new Todo(prompt('Enter a new todo:','coffee plz'), "6H"));
+    }
+}
+
+@observer
+class TodoView extends React.Component {
+    render() {
+        return (<div onDoubleClick={ this.xuyName }>
+                    { this.props.todo.duration }
+                </div>);
+    }
+    xuyName = () => {
+        this.props.todo.duration = this.props.todo.duration + "XUY";
+    }
+}
+ReactDOM.render(<App store={ list } />, document.getElementById('app'));
